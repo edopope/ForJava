d1d4f4bda2890d01a3467d0d429deef8e1245a67
Edopope <ailenontorgodwin@gmail.com>
2023-01-21 16:01:42 +0100

    another commit

---
+++ src/InterviewQuestions/DuplicateString.java
@@ -0,0 +1,25 @@
+package Activities;
+
+import java.util.Arrays;
+
+public class DuplicateString {
+    public static void main(String[] args) {
+       String name = "my boy is good and wise";
+       char[] arrayName = name.toCharArray();
+       int count;
+        for (int i = 0; i < arrayName.length ; i++) {
+            count = 1;
+            int j;
+            for (j = i + 1; j < arrayName.length; j++) {
+                if (arrayName[i] == arrayName[j] && arrayName[i] != ' ') {
+                    count++;
+                    //arrayName[j] = '0';
+                }
+            }
+            if (count > 1 && arrayName[i] != '0')
+                System.out.print(arrayName[i]);
+
+        }
+        }
+    }
+//}
